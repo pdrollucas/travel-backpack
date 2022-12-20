022f76d9d278bff6df8d56193181c7d867344c65
Pedro Lucas <pedrolucasluckow@hotmail.com>
2022-12-20 11:20:05 -0300

    cleaning an item

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ form.addEventListener("submit", (evento) => {
 
         atualizaElemento(itemAtual)
 
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id + 1: 0;
 
         criaElemento(itemAtual)
     
@@ -54,9 +54,31 @@ function criaElemento (item){
 
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild((botaoDeleta)(item.id))
+
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item){
     document.querySelector("[data-id = '"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "x"
+
+    elementoBotao.addEventListener("click", function (){
+        deletaElemento(this.parentNode)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento (tag, id){
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id) , 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
 }
